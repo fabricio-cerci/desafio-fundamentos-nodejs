65485ddd81033492bf2c3c7534d241ba07ed0ccc
Fabricio Cerci <fabriciocerci@gmail.com>
2020-05-16 17:55:06 -0400

    Verify if outcome value is bigger than total value

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Outcome value bigger than total existent value');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
